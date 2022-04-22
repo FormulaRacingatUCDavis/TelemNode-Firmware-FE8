abffc2df000be23b2bfd895ba0091ddc91499051
OPbream <100049479+OPbream@users.noreply.github.com>
2022-04-21 18:56:43 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,105 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.7
+        Device            :  PIC18F26K83
+        Driver Version    :  2.00
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#include "mcc_generated_files/mcc.h"
+#include "mcc_generated_files/pin_manager.h"
+#include "mcc_generated_files/ecan.h"
+#include "mcc_generated_files/adcc.h"
+
+/*
+                         Main application
+ */
+void main(void)
+{
+    // Initialize the device
+    SYSTEM_Initialize();
+    
+     uCAN_MSG Msg;
+    
+    Msg.frame.idType = 0x00;
+    Msg.frame.id = 0x470; //just a replacement here, probably change the id after look at the documentation for Telem node
+    Msg.frame.dlc = 0x02; // num of bytes, may need to change
+     
+    //the pin that arrow point at on telemnode and pckkit3 connect together
+    
+    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
+    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global Interrupts
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    //INTERRUPT_GlobalInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    while (1)
+    {
+        // Add your application code
+    
+    
+    ADCC_StartConversion(channel_ANC0);
+   while (!ADCC_IsConversionDone()){
+       ADCC_StopConversion();
+   }
+    adc_result_t Result;
+    ADCC_GetConversionResult();
+    //printf("%d %d", resultHIGH,resultLOW);
+    
+  
+    
+   
+    //place holders, change the value if needed
+    Msg.frame.data0 = ADRESH;
+    Msg.frame.data1 = ADRESL;
+    //Msg.frame.data2 = 0xAA;
+    //Msg.frame.data3 = 0xAA;
+    
+    CAN_transmit(&Msg);
+    
+    __delay_ms(500);
+    
+    }
+}
+/**
+ End of File
+*/
